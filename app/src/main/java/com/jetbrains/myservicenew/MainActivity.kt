e6d6c859b6040f2b627709171884a5522c061104
yudhakautsar <muhammad.kautsar@iconpln.co.id>
2022-11-09 14:07:37 +0700

    Add Foreground Service

--- app/src/main/java/com/jetbrains/myservicenew/MainActivity.kt
+++ app/src/main/java/com/jetbrains/myservicenew/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jetbrains.myservicenew
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jetbrains.myservicenew.databinding.ActivityMainBinding
@@ -15,12 +16,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+
         binding.apply {
             btnStartBackgroundService.setOnClickListener {
                 startService(serviceIntent)
             }
             btnStopBackgroundService.setOnClickListener {
                 stopService(serviceIntent)
+            }
+
+            btnStartForegroundService.setOnClickListener {
+                if (Build.VERSION.SDK_INT >= 26) {
+                    startForegroundService(foregroundServiceIntent)
+                } else {
+                    startService(foregroundServiceIntent)
+                }
+            }
+            btnStopForegroundService.setOnClickListener {
+                stopService(foregroundServiceIntent)
             }
         }
     }
